bdbed0efa217f418bef0f7483ee37670a66bac16
Muhammad Ruby Muhammad <93392046+MuhammadRuby@users.noreply.github.com>
2023-08-08 17:23:39 +0300

    Update 0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -4,7 +4,7 @@
 def match_school(argument)
   pattern = /School/i  # The 'i' flag makes the pattern case-insensitive
   matched_text = argument.scan(pattern).join
-  puts "#{matched_text}$"
+  puts matched_text
 end
 
 # Get the command-line argument
